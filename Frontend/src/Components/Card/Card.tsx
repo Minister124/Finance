871e4f1c5f560f9363e42e2398ccaf7d5b5a1b7f
Minister124 <kushaltmg321@gmail.com>
2024-09-08 14:55:49 +0545

    Comments

--- Frontend/src/Components/Card/Card.tsx
+++ Frontend/src/Components/Card/Card.tsx
@@ -7,7 +7,7 @@ interface Props {
   Description: string;
 }
 
-const Card : React.FC<Props> = ({ ImageLink, CompanyName, Price, Description }) => {
+const Card : React.FC<Props> = ({ ImageLink, CompanyName, Price, Description }) => { //Can have : JSX.Element but does not matter as it automatically accepts as it is even if not specified
   return (
     <div className="">
       <img
