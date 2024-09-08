319cd4389002b02ed8f065daa24bc8fafae7ceb5
Minister124 <kushaltmg321@gmail.com>
2024-09-08 22:18:41 +0545

    api error

--- Frontend/src/api.tsx
+++ Frontend/src/api.tsx
@@ -2,16 +2,26 @@ import axios from "axios";
 import { CompanySearch } from "./CompanyData";
 
 export interface SearchResponse {
-    data: CompanySearch[];
+  data: CompanySearch[];
 }
 
 export const SearchCompanies = async (query: string) => {
-    try{
-        const data = await axios.get<SearchResponse>(`https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`);
-        return data;
-    } catch(error){
-        if (axios.isAxiosError(error)) {
-            
-        }
+  try {
+    const data = await axios.get<SearchResponse>(
+      `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error: any) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.error("Error response:", error.response);
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.error("Error request:", error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.error("Error message:", error.message);
     }
+  }
 };
