85e2932fa8af5a2278866c9a8a1353da7e862e81
Minister124 <kushaltmg321@gmail.com>
2024-09-08 19:53:22 +0545

    Events

--- Frontend/src/Components/Search/Search.tsx
+++ Frontend/src/Components/Search/Search.tsx
@@ -1,25 +1,29 @@
-import React, { ChangeEvent, useState } from "react";
+import React, {
+  ChangeEvent,
+  MouseEvent,
+  useState
+} from "react";
 
-const Search: React.FC<{}> = () => {
+const Search: React.FC<{}> = (): JSX.Element => {
   const [search, setSearch] = useState<string>("");
 
-//   const onClick = (e: any) =>{ //any is whatever there in string
-//     setSearch(e.target.value);
-//     console.log(e);
-//   } 
+  //   const onClick = (e: any) =>{ //any is whatever there in string
+  //     setSearch(e.target.value);
+  //     console.log(e);
+  //   }
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) =>{ 
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
     console.log(e);
-  } 
+  };
 
-  const onClick = () =>{
-    
-  }
+  const onClick = (e: MouseEvent<HTMLButtonElement>) => {
+    console.log(e);
+  };
   return (
     <div>
-      <input value={search} onChange={(e) => handleChange(e)} />
-      <button onClick={onClick}></button>
+      <input className="border-4" value={search} onChange={(e) => handleChange(e)} />
+      <button className="w-4 mb-10 h-6 border-2" onClick={(e) => onClick(e)}/>
     </div>
   );
 };
