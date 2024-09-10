807523e9438be8b84650a3dfb25ce17992714142
Minister124 <kushaltmg321@gmail.com>
2024-09-10 14:44:38 +0545

    Financial prep api

--- Frontend/src/api.tsx
+++ Frontend/src/api.tsx
@@ -16,12 +16,15 @@ export const SearchCompanies = async (query: string) => {
       // The request was made and the server responded with a status code
       // that falls out of the range of 2xx
       console.error("Error response:", error.response);
+      console.log("Error response:", error.response);
     } else if (error.request) {
       // The request was made but no response was received
       console.error("Error request:", error.request);
+      console.log("Error request:", error.request);
     } else {
       // Something happened in setting up the request that triggered an Error
       console.error("Error message:", error.message);
+      console.log("Error message:", error.message);
     }
   }
 };
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,7 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { SearchCompanies } from './api.tsx'
 
+console.log(SearchCompanies("tesla"));
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
